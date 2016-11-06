4daf5b137703144ef32dbc651701aa18854e90bc
jneilliii <jneilliii+github@gmail.com>
2016-11-06 01:44:41 -0400

    Initial release.

---
+++ octoprint_ipOnHeader/__init__.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+import socket
+
+class ipOnHeaderPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.SettingsPlugin):
+	def on_startup(self):
+		self._logger.info("ipOnHeaderPlugin: " + [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
+		self._settings.global_set(["appearance","name"],[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
+		self._settings.save()
+
+	##~~ Softwareupdate hook
+	def get_update_information(self):
+		return dict(
+			ipOnHeader=dict(
+				displayName="ipOnHeader Plugin",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="jneilliii",
+				repo="OctoPrint-ipOnHeader",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/jneilliii/OctoPrint-ipOnHeader/archive/{target_version}.zip"
+			)
+		)
+
+__plugin_name__ = "ipOnHeader Plugin"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = ipOnHeaderPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "ipOnConnect"
+plugin_identifier = "ipOnHeader"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_ipOnConnect"
+plugin_package = "octoprint_ipOnHeader"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-ipOnConnect"
+plugin_name = "OctoPrint-ipOnHeader"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1.0"
@@ -27,7 +27,7 @@ plugin_author = "jneilliii"
 plugin_author_email = "jneilliii@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/jneilliii/OctoPrint-ipOnConnect"
+plugin_url = "https://github.com/jneilliii/OctoPrint-ipOnHeader"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
